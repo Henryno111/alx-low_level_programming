e462c3f0cbc849032ad4c2ce8a68509275f2c6a3
agukwe henry <h.agukwe@yahoo.com>
2023-02-17 05:17:48 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main entry point
+/**
+ * main - this prints the statementi c
+ *
+ * Return: always 0
+ */
+
+int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
