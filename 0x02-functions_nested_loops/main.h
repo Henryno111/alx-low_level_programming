148bf86af43d5f979add9472934f574e8588bcb9
agukwe henry <h.agukwe@yahoo.com>
2023-02-22 16:03:37 -0800

    task 14

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,5 +11,8 @@ int print_last_digit(int);
 void jack_bauer(void);
 void print_to_98(int n);
 int _putchar(char c);
+void times_table(void);
+int add(int, int);
+void print_times_table(int n);
 
 #endif
